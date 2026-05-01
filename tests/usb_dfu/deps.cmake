f7de56e1800495e832361547300e122005b40b75
Humphrey Bucknell <humphreybucknell@xmos.com>
2026-05-01 08:57:57 +0100

    Update dependency

--- tests/usb_dfu/deps.cmake
+++ tests/usb_dfu/deps.cmake
@@ -1,3 +1,3 @@
-set(APP_DEPENDENT_MODULES "humphrey-xmos/lib_xua(feature/dfu-transition)"
+set(APP_DEPENDENT_MODULES "lib_xua(develop)"
                           "lib_i2s(6.0.1)"
                           "lib_board_support(1.5.0)")
